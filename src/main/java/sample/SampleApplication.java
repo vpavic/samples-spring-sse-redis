6973106a5bbe725218bfa65de41cc7e8a8939dd0
Vedran Pavic <vedran.pavic@gmail.com>
2021-11-30 23:42:37 +0100

    Initial commit

---
+++ src/main/java/sample/SampleApplication.java
@@ -0,0 +1,94 @@
+package sample;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveSubscription;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication(proxyBeanMethods = false)
+@RestController
+public class SampleApplication {
+
+    private static final String SAMPLE_CHANNEL = "sample:events";
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @Autowired
+    private ReactiveRedisOperations<String, Event> eventRedisOperations;
+
+    @PostMapping("/events")
+    Mono<Map<String, Long>> createEvent() {
+        return this.eventRedisOperations.convertAndSend(SAMPLE_CHANNEL, Event.generate())
+                .map(count -> Map.of("subscriberCount", count));
+    }
+
+    @GetMapping(path = "/events")
+    Flux<ServerSentEvent<Event>> getEvents() {
+        return this.eventRedisOperations.listenToChannel(SAMPLE_CHANNEL)
+                .map(ReactiveSubscription.Message::getMessage)
+                .map(event -> ServerSentEvent.builder(event).id(event.id.toString()).event(event.type).build())
+                .mergeWith(Flux.interval(Duration.ZERO, Duration.ofSeconds(15L))
+                        .map(i -> ServerSentEvent.<Event>builder().comment("keepalive").build()));
+    }
+
+    static class Event {
+
+        @JsonProperty
+        private final UUID id;
+
+        @JsonProperty
+        private final String type;
+
+        Event(UUID id, String type) {
+            this.id = id;
+            this.type = type;
+        }
+
+        static Event generate() {
+            return new Event(UUID.randomUUID(), "sample");
+        }
+
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    static class RedisConfiguration {
+
+        @Bean
+        ReactiveRedisOperations<String, Event> eventRedisOperations(
+                ReactiveRedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+            Jackson2JsonRedisSerializer<Event> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Event.class);
+            jsonRedisSerializer.setObjectMapper(objectMapper);
+            return new ReactiveRedisTemplate<>(redisConnectionFactory,
+                    RedisSerializationContext.<String, Event>newSerializationContext()
+                            .key(RedisSerializer.string())
+                            .value(jsonRedisSerializer)
+                            .hashKey(RedisSerializer.string())
+                            .hashValue(jsonRedisSerializer)
+                            .build());
+        }
+
+    }
+
+}
