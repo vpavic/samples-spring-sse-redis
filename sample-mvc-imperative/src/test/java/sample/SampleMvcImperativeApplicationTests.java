59c53b466894b52550211569030fdb6f6c83fa3c
Vedran Pavic <vpavic@duck.com>
2021-12-15 23:49:30 +0100

    Mock Redis connection factory in tests

--- sample-mvc-imperative/src/test/java/sample/SampleMvcImperativeApplicationTests.java
+++ sample-mvc-imperative/src/test/java/sample/SampleMvcImperativeApplicationTests.java
@@ -2,9 +2,15 @@ package sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootTest
 class SampleMvcImperativeApplicationTests {
+
+    @MockBean
+    @SuppressWarnings("unused")
+    private RedisConnectionFactory redisConnectionFactory;
 
     @Test
     void contextLoads() {
--- sample-webflux/src/test/java/sample/SampleWebFluxApplicationTests.java
+++ sample-webflux/src/test/java/sample/SampleWebFluxApplicationTests.java
@@ -2,9 +2,20 @@ package sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootTest
 class SampleWebFluxApplicationTests {
+
+    @MockBean
+    @SuppressWarnings("unused")
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockBean
+    @SuppressWarnings("unused")
+    private ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
 
     @Test
     void contextLoads() {
